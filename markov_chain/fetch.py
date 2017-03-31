a068a5c1c3929b6a0ea7f1fffc4253ed8bfbf481
mudspringhiker <thirdwave72@gmail.com>
2017-03-31 09:53:12 -0500

    Create save function

--- markov_chain/fetch.py
+++ markov_chain/fetch.py
@@ -2,34 +2,25 @@ import requests
 import bs4
 import os
 
-url = "http://concordpastor.blogspot.com"
 
 def get_text_from_web(url):
 
     response = requests.get(url)
     soup = bs4.BeautifulSoup(response.text, 'html.parser')
-    post = soup.find(id = 'main-wrapper').find(class_='post-body').get_text()
+    post = soup.find(id = 'main-wrapper').find(class_='entry-content').get_text()
+    post = post.strip().encode('utf-8')
     return post
 
-#html = get_html_from_web(url)
 
-#soup = bs4.BeautifulSoup(html, 'html.parser')
+def save(name, post):
+    filename = os.path.abspath(os.path.join(name + '.txt'))
+    with open(filename, "w") as file:
+        file.write(post)
 
-#date = soup.find(id = 'main-wrapper').find('h2').get_text()
 
-#print date
+#url = "http://concordpastor.blogspot.com"
 
-#title = soup.find(id = 'main-wrapper').find('h3').get_text()
+#text = get_text_from_web(url)
 
-#print title
-
-text = get_text_from_web(url)
-
-format_text = text.strip().split()
-print format_text
-
-#print post
-
-#def dump_post(name):
-#    filename = os.path.abspath(os.path.join(name, '.txt'))
-#    with open(filename, "w") as file:
+#print text
+#save("concordpastor", text)
